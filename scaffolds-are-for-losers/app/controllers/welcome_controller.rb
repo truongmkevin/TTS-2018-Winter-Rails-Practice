ce475f1e0ec9b4c898d238250537f82e1543f71f
KT <truongmkevin@yahoo.com>
2018-03-06 14:51:51 +0000

    initial commit for no scaffolding app

---
+++ scaffolds-are-for-losers/app/controllers/welcome_controller.rb
@@ -0,0 +1,10 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def users
+  end
+
+  def products
+  end
+end
---
+++ scaffolds-are-for-losers/test/controllers/welcome_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class WelcomeControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get welcome_index_url
+    assert_response :success
+  end
+
+  test "should get users" do
+    get welcome_users_url
+    assert_response :success
+  end
+
+end
