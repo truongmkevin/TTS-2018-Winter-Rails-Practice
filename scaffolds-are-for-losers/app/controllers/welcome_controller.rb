924f47a62e30a2c97ece64c79fb1d9b2e3cd7727
KT <truongmkevin@yahoo.com>
2018-03-06 16:34:01 +0000

    finished users, started coding for products

--- scaffolds-are-for-losers/app/controllers/welcome_controller.rb
+++ scaffolds-are-for-losers/app/controllers/welcome_controller.rb
@@ -3,8 +3,19 @@ class WelcomeController < ApplicationController
   end
 
   def users
+    if params[:name] != nil && params[:age].present?
+      user = User.create(name: params[:name], age: params[:age])
+    end
+    @users = User.all
   end
 
   def products
+  end
+
+  # Don't need this anymore because our routes skip over this method
+  # to use the if statement in the def users
+  def create_users
+    user = User.create(name: params[:name], age: params[:age])
+    redirect_to users_path
   end
 end
--- scaffolds-are-for-losers/config/routes.rb
+++ scaffolds-are-for-losers/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get 'users' => 'welcome#users'
+
+  # Commented out because slides wanted to refactor to shorter
+  # post 'create_users' => 'welcome#create_users'
+
+  post 'create_users' => 'welcome#users'
   get 'products' => 'welcome#products'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
