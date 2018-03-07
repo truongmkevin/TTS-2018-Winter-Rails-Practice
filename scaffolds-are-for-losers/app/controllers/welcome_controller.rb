10ff5475f0d4c52ac067084cca668f3f393c7ce1
KT <truongmkevin@yahoo.com>
2018-03-07 05:20:24 +0000

    end of our no scaffolds project with everything working

--- scaffolds-are-for-losers/app/controllers/welcome_controller.rb
+++ scaffolds-are-for-losers/app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @product = Product.new
   end
 
   def users
@@ -9,7 +10,13 @@ class WelcomeController < ApplicationController
     @users = User.all
   end
 
+  def create_products
+    product = Product.create(product_params)
+    redirect_to products_path
+  end
+
   def products
+    @products = Product.all
   end
 
   # Don't need this anymore because our routes skip over this method
@@ -18,4 +25,11 @@ class WelcomeController < ApplicationController
     user = User.create(name: params[:name], age: params[:age])
     redirect_to users_path
   end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:name, :price, :quantity)
+    end
+
 end
--- scaffolds-are-for-losers/config/routes.rb
+++ scaffolds-are-for-losers/config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   post 'create_users' => 'welcome#users'
   get 'products' => 'welcome#products'
+  post 'products' => 'welcome#create_products'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
